022dc5d043965f4a477b76bb7e72d575b5d58c1d
Nick-OD <odonnell.nick28@gmail.com>
2017-09-11 20:28:04 +0200

    Sphere moves on the platform now

---
+++ 2DPlatfomerAssignmentBase/Assets/SphereController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour {
+
+
+	public float speed = 10;
+	public float jumpvelocity = 10;
+	public LayerMask playerMask;
+	public bool canmoveinair = true;
+	Transform myTransform, tagGround;
+	Rigidbody2D mybody;
+	bool isground = true;
+
+
+	// Use this for initialization
+	void Start () {
+		mybody = this.GetComponent<Rigidbody2D> ();
+		myTransform = this.GetComponent<Transform>();
+		//tagGround = GameObject.Find(this.name + "/Tag Ground").transform;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		//isground = Physics2D.Linecast (myTransform.position, tagGround.position, playerMask);
+		Move (Input.GetAxisRaw("Horizontal"));
+		if (Input.GetButtonDown ("Jump")) {
+			Jump ();
+		}
+	}
+
+
+	public void Move(float horzontalInput){
+		if (!canmoveinair && !isground)
+			return;
+		Vector2 moveVel = mybody.velocity;
+		moveVel.x = horzontalInput * speed;
+		mybody.velocity = moveVel;
+
+	}
+
+	public void Jump(){
+		if (isground)
+			mybody.velocity += jumpvelocity * Vector2.up;
+
+	}
+}
